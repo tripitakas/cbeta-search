9f5366f1de49cd27a99228be6aae0dcbaac0c9a2
Chao Li <lichaorodxx@gmail.com>
2019-07-06 16:49:11 -0700

    done #4
    Now we are able to start the service with ES and Kibana optionally
    running in docker container.

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ import signal
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application
 from tornado.options import define, options
-from search import build_db
+from search import build_db, BM_PATH, TXT_PATH
 
 
 define('port', default=8020, help='run port', type=int)
+define('docker_indexer', default=False, 
+       help='Whether start Elasticsearch in docker container')
+define('run_kibana', default=False,
+       help='Whether start kibana in a docker container')
+define('BM_PATH', default=BM_PATH)
 
 
 class MainHandler(RequestHandler):
@@ -37,6 +42,7 @@ def make_app():
 
 
 docker_client = docker.from_env()
+running_containers = []
 
 
 def stop_es_container():
@@ -45,18 +51,29 @@ def stop_es_container():
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGTERM, stop_es_container)
-    # First try building the es-ik-tripitakas docker image
-    print('Try building es-ik-tripitakas image')
-    (image, obj) = docker_client.images.build(path='./es-ik')
-    # Second, start elasticsearch container
-    print('Try running es-ik-tripitakas image')
-    docker_client.containers.run(image, detach=True)
+    options.parse_command_line()
+    BM_PATH = options.BM_PATH
+    # If running indexer in docker, otherwise assuming ES already started locally
+    if options.docker_indexer:
+        signal.signal(signal.SIGTERM, stop_es_container)
+        # First try building the es-ik-tripitakas docker image
+        print('Try building es-ik-tripitakas image')
+        (image, obj) = docker_client.images.build(path='./es-ik')
+        # Second, start elasticsearch container
+        print('Try running es-ik-tripitakas image')
+        running_containers.append(docker_client.containers.run(
+            image, environment=['discovery.type=single-node'],
+            ports={'9300/tcp': 9300, '9200/tcp': 9200},
+            detach=True))
+    if options.run_kibana:
+        print('starting kibana docker container')
+        running_containers.append(docker_client.containers.run(
+            'docker.elastic.co/kibana/kibana:7.2.0', ports={'5601/tcp': 5601}, 
+            detach=True))
     print('Building elasticsearch index from ocr files')
     build_db()
 
     # Then start tornado IO loop
-    options.parse_command_line()
     app = make_app()
     print('Start the app on http://localhost:%d' % (options.port,))
     app.listen(options.port)
